2ca6e91d17ef7b47051b8977f2319c301154f190
dev_20nov@hotmail.com <dev_20nov@hotmail.com>
2020-12-02 14:12:52 +0900

    paiza to github

---
+++ AtCoder_Contest_A/ABC180_box/200802/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Your code here!
+        
+        int box = 100;
+        int a = 1;
+        int b = 2;
+        
+        // N個のボールが入っている箱からA個取り出して、B個入れる
+        int result = box - a + b;
+        
+        System.out.println(result);
+    }
+}
